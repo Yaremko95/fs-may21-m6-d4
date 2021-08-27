34c5322c66b1006b9a8fa50994ffb8c741b2a0e7
tetiana <tetianayaremko@gmail.com>
2021-08-27 10:39:41 +0300

    adds config for deployment

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,17 @@
-import { Sequelize } from "sequelize";
+import s from "sequelize";
+const { Sequelize, QueryTypes } = s;
 
 const { PGDATABASE, PGUSER, PGPASSWORD, PGHOST } = process.env;
 
 const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
   host: PGHOST,
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      required: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 const schemas = ["development", "production"]
@@ -17,7 +24,7 @@ console.log(schemas);
 export const syncSequelize = async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.query(schemas);
+    await sequelize.query(schemas, { type: QueryTypes.SELECT });
     await sequelize.sync({
       //   force: true,
       logging: false,
